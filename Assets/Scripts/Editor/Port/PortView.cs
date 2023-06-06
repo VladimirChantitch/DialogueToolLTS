ba3ca09646c90826b86e8e780c4e3d7a0d418edf
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-06-06 17:48:08 +0200

    Deprivatisation of unity Code

--- Assets/Scripts/Editor/Port/PortView.cs
+++ Assets/Scripts/Editor/Port/PortView.cs
@@ -3,20 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.Experimental.GraphView.Port;
 
 public class PortView : Port
 {
-    protected PortView(Orientation portOrientation, Direction portDirection, Capacity portCapacity, Type type) : base(portOrientation, portDirection, portCapacity, type)
-    {
-
-    }
-
-    public void Init(PortType portType)
+    protected PortView(Orientation portOrientation, Direction portDirection, Capacity portCapacity, Type type, PortType portType) : base(portOrientation, portDirection, portCapacity, type)
     {
         this.portType = portType;
     }
 
     public PortType portType;
+
+    public static PortView Create<TEdge>(Orientation orientation, Direction direction, Capacity capacity, Type type, PortType portType) where TEdge : Edge, new()
+    {
+        EdgeConnectorListener listener = new EdgeConnectorListener();
+        PortView port = new PortView(orientation, direction, capacity, type, portType)
+        {
+            m_EdgeConnector = new EdgeConnector<TEdge>(listener)
+        };
+        port.AddManipulator(port.m_EdgeConnector);
+        return port;
+    }
 }
 
 public class PortType
